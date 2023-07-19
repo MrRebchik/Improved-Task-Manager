3c7a6131d2302accfd6f867670815b720dfd08bd
Baskoibanes <vovazms@gmail.com>
2023-07-19 16:13:24 +0300

    Added first step of signing up (template and view)

--- python/main_site/authorization/views.py
+++ python/main_site/authorization/views.py
@@ -1,7 +1,12 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 
+
+from django.contrib.auth.forms import UserCreationForm
+
+
 # Create your views here.
 
-def index(request):
-    return HttpResponse('<p>Skin chlen</p>')
+def registration(request):
+    user_reg_form = UserCreationForm()
+    return render(request, 'authorization/registration.html', {'form': user_reg_form})
