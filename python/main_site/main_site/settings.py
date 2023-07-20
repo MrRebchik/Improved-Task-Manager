190825cbdcaa882c04c17c3aed2cef360955cc50
Baskoibanes <vovazms@gmail.com>
2023-07-20 12:20:17 +0300

    Added connection to sqlite db

--- python/main_site/main_site/settings.py
+++ python/main_site/main_site/settings.py
@@ -78,7 +78,7 @@ WSGI_APPLICATION = 'main_site.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'NAME': BASE_DIR.parent.parent / 'db.sqlite3',
     }
 }
 
