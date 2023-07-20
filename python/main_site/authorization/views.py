b693a20d84413018e1e8186fe666da5269d8e21d
Baskoibanes <vovazms@gmail.com>
2023-07-20 12:42:41 +0300

    Added primitive registration

--- python/main_site/authorization/views.py
+++ python/main_site/authorization/views.py
@@ -1,12 +1,21 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 
-
+from django.contrib import messages
 from django.contrib.auth.forms import UserCreationForm
 
 
 # Create your views here.
 
 def registration(request):
-    user_reg_form = UserCreationForm()
-    return render(request, 'authorization/registration.html', {'form': user_reg_form})
+    if request.method == 'POST':
+        user_reg_form = UserCreationForm(request.POST)
+        if user_reg_form.is_valid():
+            user_reg_form.save()
+            messages.success(request, 'Успешно рега')
+            return HttpResponse('<h1>Govno</h1>')
+        else:
+            return HttpResponse('<p>Вы обосрались</p>')
+    else:
+        user_reg_form = UserCreationForm()
+        return render(request, 'authorization/registration.html', {'form': user_reg_form})
