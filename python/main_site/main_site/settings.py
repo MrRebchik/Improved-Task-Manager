f2ed02881cd69e2a06683802332f836cc576b34c
Baskoibanes <vovazms@gmail.com>
2023-07-25 00:09:56 +0300

    Added authomatic tasks listing and anchor for task creation

--- python/main_site/main_site/settings.py
+++ python/main_site/main_site/settings.py
@@ -38,6 +38,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'authorization.apps.AuthorizationConfig',
+    'task_viewer.apps.TaskViewerConfig',
 ]
 
 MIDDLEWARE = [
