11cf23f3f970ab466916d13bf6e9e6101ff5cd84
Baskoibanes <vovazms@gmail.com>
2023-08-10 11:11:29 +0300

    Added title block in templates, changed task_reading out of CRUD, fixed redirect after signing in

--- python/main_site/task_viewer/urls.py
+++ python/main_site/task_viewer/urls.py
@@ -1,9 +1,9 @@
 from django.urls import path
 
-from .views import task_list, task_create, task_delete
+from .views import task_read, task_create, task_delete
 
 urlpatterns = [
-    path('tasks/', task_list, name='task_list'),
+    path('tasks/', task_read, name='task_read'),
     path('tasks/create', task_create, name='task_create'),
     path('tasks/delete/<str:id>', task_delete, name='task_delete'),
 ]
