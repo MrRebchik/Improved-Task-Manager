1875bb75374ab55b764696fe73245310d0a555ac
Baskoibanes <vovazms@gmail.com>
2023-08-24 20:43:29 +0300

    Implemented module integration

--- golang/auth/repository.go
+++ golang/auth/repository.go
@@ -1,1 +1,9 @@
-package repository
+package auth
+
+import (
+	user "github.com/MrRebchik/Improved-Task-Manager/models"
+)
+
+type Authorization interface {
+	user.User
+}
--- golang/go.mod
+++ golang/go.mod
@@ -1,3 +1,3 @@
-module golang
+module github.com/MrRebchik/Improved-Task-Manager
 
 go 1.21.0
