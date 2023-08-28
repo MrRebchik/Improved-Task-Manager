577279f3968166be13ebcad3b93dfe867662407e
Baskoibanes <vovazms@gmail.com>
2023-08-28 10:56:10 +0300

    Added converting of user model types

--- golang/models/users.go
+++ golang/models/users.go
@@ -1,7 +1,24 @@
 package models
 
+import "github.com/MrRebchik/Improved-Task-Manager/auth/repository/mongo"
+
 type User struct {
 	ID       string
 	Username string
 	Password string
+}
+
+func toMongoUser(u *User) *mongo.User {
+	return &mongo.User{
+		Username: u.Username,
+		Password: u.Password,
+	}
+}
+
+func toModelUser(u *mongo.User) *User {
+	return &User{
+		ID:       u.ID.Hex(),
+		Username: u.Username,
+		Password: u.Password,
+	}
 }
