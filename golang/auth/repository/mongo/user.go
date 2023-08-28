650bbb29a9b7a81159aec1bbefd36d332332ba5c
Baskoibanes <vovazms@gmail.com>
2023-08-28 10:59:12 +0300

    Added repository management methods

--- golang/auth/repository/mongo/user.go
+++ golang/auth/repository/mongo/user.go
@@ -1,8 +1,11 @@
 package mongo
 
 import (
+	"context"
 	models "github.com/MrRebchik/Improved-Task-Manager/models"
+	"go.mongodb.org/mongo-driver/bson"
 	"go.mongodb.org/mongo-driver/bson/primitive"
+	"go.mongodb.org/mongo-driver/mongo"
 )
 
 type User struct {
@@ -24,4 +27,33 @@ func toModelUser(user *User) *models.User {
 		Username: user.Username,
 		Password: user.Password,
 	}
+}
+
+type UserRepository struct {
+	db *mongo.Collection
+}
+
+func (r UserRepository) CreateUser(ctx context.Context, user *models.User) error {
+	mongoModel := toMongoUser(user)
+	res, err := r.db.InsertOne(ctx, mongoModel)
+	if err != nil {
+		return err
+	}
+
+	user.ID = res.InsertedID.(primitive.ObjectID).Hex()
+	return nil
+}
+
+func (r UserRepository) GetUser(ctx context.Context, username, password string) (*models.User, error) {
+	user := new(User)
+	err := r.db.FindOne(ctx, bson.M{
+		"username": username,
+		"password": password,
+	}).Decode(user)
+
+	if err != nil {
+		return nil, err
+	}
+
+	return models.ToModelUser(user), nil
 }
