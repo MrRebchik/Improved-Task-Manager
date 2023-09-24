2c4c703db682970d044ee395ea2b9d49e6ba0d0a
Baskoibanes <vovazms@gmail.com>
2023-09-24 19:06:11 +0300

    Added first gin HttpHandler implementation

--- golang/authorization/pkg/handler/handler.go
+++ golang/authorization/pkg/handler/handler.go
@@ -1,10 +1,26 @@
 package handler
 
 import (
+	"authorization/pkg/service"
 	"context"
 )
 
 type HttpHandler interface {
 	GetUser(ctx *context.Context)
 	CreateUser(ctx *context.Context)
+}
+
+type BrokerHandler interface {
+	// TODO methods
+}
+
+type Handler struct {
+	HttpHandler
+	BrokerHandler
+}
+
+func NewHandler(services service.Service) *Handler {
+	return &Handler{
+		// TODO Handler return
+	}
 }
--- golang/authorization/pkg/handler/http/gin_handler.go
+++ golang/authorization/pkg/handler/http/gin_handler.go
@@ -1,7 +1,28 @@
 package handler
 
-import "authorization/pkg/service"
+import (
+	"authorization/pkg/service"
+	"github.com/gin-gonic/gin"
+)
 
-type HttpHandler struct {
+type GinHttpHandler struct {
 	services *service.Service
+}
+
+func NewGinHttpHandler(services *service.Service) *GinHttpHandler {
+	return &GinHttpHandler{
+		services: services,
+	}
+}
+
+func (h *GinHttpHandler) InitRoutes() *gin.Engine {
+	g := gin.New()
+
+	auth := g.Group("/auth")
+	{
+		auth.POST("/sign-in")
+		auth.POST("sign-up")
+	}
+
+	return g
 }
