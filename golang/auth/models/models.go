570d1841f12cca9597cdfdaaa0a3bec866db722f
Baskoibanes <vovazms@gmail.com>
2023-09-29 23:11:03 +0300

    Added UnmarshallJSON method to user model

--- golang/auth/models/models.go
+++ golang/auth/models/models.go
@@ -1,7 +1,25 @@
 package models
 
+import (
+	"encoding/json"
+	"github.com/sirupsen/logrus"
+)
+
 type User struct {
 	ID       string `json:"id,omitempty"`
 	Username string `json:"username"`
 	Password string `json:"password"`
+}
+
+func (u *User) UnmarshalJSONToUser(userJSON []byte) error {
+	err := json.Unmarshal(userJSON, &u)
+
+	if err != nil {
+		logrus.Errorln("Error while unmarshalling userJSON")
+		return err
+	}
+
+	logrus.Infoln("Unmarshalled user ", u.Username)
+
+	return nil
 }
