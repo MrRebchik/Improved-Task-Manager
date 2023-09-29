333c57db74d21032210833f0b74445cb7537ef4a
Baskoibanes <vovazms@gmail.com>
2023-09-29 23:49:23 +0300

    Created server script

--- golang/auth/server/server.go
+++ golang/auth/server/server.go
@@ -1,1 +1,24 @@
 package server
+
+import (
+	"github.com/sirupsen/logrus"
+	"net/http"
+)
+
+type Server struct {
+	httpServer *http.Server
+}
+
+func (s *Server) Run(port string, handler http.Handler) error {
+	s.httpServer = &http.Server{
+		Handler: handler,
+		Addr:    ":" + port,
+	}
+	err := s.httpServer.ListenAndServe()
+
+	if err != nil {
+		logrus.Errorln("Error while starting server on port ", port)
+		return err
+	}
+	return nil
+}
